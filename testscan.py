f0c74df8a76b3f3bee31ae1afc83389bddd13c83
Aiba Masamitsu <masamitsu.aiba@psi.ch>
2015-10-19 17:04:29 +0200

    Example script and Manual are added.

---
+++ testscan.py
@@ -0,0 +1,67 @@
+from pyScan import *
+
+pyscan=Scan()
+
+
+
+indict1={}
+indict1['Knob']=['VA-SARUN04-UIND030-MOT:X-SET']
+indict1['KnobReadback']=['VA-SARUN04-UIND030-MOT:X-READ']
+indict1['KnobTolerance']=[0.01]
+indict1['KnobWaiting']=[10]
+#indict0['KnobWaitingExtra']=0
+
+indict1['ScanRange']=[[-0.3,0.3]]
+indict1['Nstep']=10
+
+#indict1['StepSize']=
+#indict1['Scan Values']=
+
+
+indict1['Observable']=['VA-SARUN06-DBPM070:X','VA-SARUN06-DBPM070:Y']
+
+indict1['Waiting']=0.1
+indict1['Validation']=[]
+
+indict1['NumberOfMeasurements']=3
+
+indict1['Monitor']=['VA-SINSS-LPSA:SHUTTER']
+indict1['MonitorValue']=['Open']
+indict1['MonitorTolerance']=[0]
+indict1['MonitorAction']=['WaitAndAbort']
+indict1['MonitorTimeout']=[5]
+
+
+indict1['PreAction']=[['VA-SARUN04-MQUA020-MOT:X','VA-SARUN04-MQUA020-MOT:X',1,0.1,10]]
+#indict1['PreActionWaiting']=0
+#indict1['PreActionOrder']=0
+
+indict1['PostAction']=[['VA-SARUN04-MQUA020-MOT:X','VA-SARUN04-MQUA020-MOT:X',0,0,10]]
+
+indict0={}
+indict0['Knob']=['VA-SARUN04-UIND030-MOT:Y-SET']
+indict0['KnobReadback']=['VA-SARUN04-UIND030-MOT:Y-READ']
+indict0['KnobTolerance']=[0.01]
+indict0['KnobWaiting']=[10]
+indict0['ScanRange']=[[-0.2,0.2]]
+indict0['Nstep']=4
+
+outdict=pyscan.initializeScan([indict0,indict1])
+
+print outdict['ErrorMessage']
+print indict1['KnobExpanded']
+
+outdict=pyscan.startScan()
+
+
+for o in  outdict['Observable']:
+    print o
+
+
+#print pyscan.outdict['KnobReadback']
+outdict=pyscan.initializeScan([indict0,indict1])
+outdict=pyscan.startScan()
+
+
+for o in  outdict['Observable']:
+    print o

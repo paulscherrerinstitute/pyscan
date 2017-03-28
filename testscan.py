b33aeff8d18d33edd20a2cb2cbc84a13afe553dd
ebner <simon.ebner@psi.ch>
2017-03-28 16:40:24 +0200

    Reformat test code

--- testscan.py
+++ testscan.py
@@ -1,71 +1,62 @@
 from pyScan import *
 
-pyscan=Scan()
+pyscan = Scan()
 
+indict1 = dict()
+indict1['Knob'] = ['MA-SARUN04-UIND030-MOT:X-SET']
+indict1['KnobReadback'] = ['MA-SARUN04-UIND030-MOT:X-SET']
+indict1['KnobTolerance'] = [0.01]
+indict1['KnobWaiting'] = [10]
+# indict0['KnobWaitingExtra']=0
 
+indict1['ScanRange'] = [[-0.3, 0.3]]
+indict1['Nstep'] = 10
 
-indict1={}
-indict1['Knob']=['MA-SARUN04-UIND030-MOT:X-SET']
-indict1['KnobReadback']=['MA-SARUN04-UIND030-MOT:X-SET']
-indict1['KnobTolerance']=[0.01]
-indict1['KnobWaiting']=[10]
-#indict0['KnobWaitingExtra']=0
+# indict1['StepSize']=
+# indict1['Scan Values']=
 
-indict1['ScanRange']=[[-0.3,0.3]]
-indict1['Nstep']=10
+indict1['Observable'] = ['MA-SARUN06-DBPM070:X', 'MA-SARUN06-DBPM070:Y']
 
-#indict1['StepSize']=
-#indict1['Scan Values']=
+indict1['Waiting'] = 0.1
+indict1['Validation'] = ['MA-SARUN06-DBPM070:POS-VALID', 'MA-SARUN06-DBPM070:Q-VALID']
 
+indict1['NumberOfMeasurements'] = 3
 
-indict1['Observable']=['MA-SARUN06-DBPM070:X','MA-SARUN06-DBPM070:Y']
+indict1['Monitor'] = ['MA-SINSS-LPSA:SHUTTER']
+indict1['MonitorValue'] = ['Open']
+indict1['MonitorTolerance'] = [0]
+indict1['MonitorAction'] = ['WaitAndAbort']
+indict1['MonitorTimeout'] = [5]
 
-indict1['Waiting']=0.1
-indict1['Validation']=['MA-SARUN06-DBPM070:POS-VALID','MA-SARUN06-DBPM070:Q-VALID']
+indict1['PreAction'] = [['MA-SARUN04-MQUA020-MOT:X', 'MA-SARUN04-MQUA020-MOT:X', 1, 0.1, 10]]
+# indict1['PreActionWaiting']=0
+# indict1['PreActionOrder']=0
 
-indict1['NumberOfMeasurements']=3
+indict1['PostAction'] = [['MA-SARUN04-MQUA020-MOT:X', 'MA-SARUN04-MQUA020-MOT:X', 0, 0, 10]]
 
-indict1['Monitor']=['MA-SINSS-LPSA:SHUTTER']
-indict1['MonitorValue']=['Open']
-indict1['MonitorTolerance']=[0]
-indict1['MonitorAction']=['WaitAndAbort']
-indict1['MonitorTimeout']=[5]
+indict0 = dict()
+indict0['Knob'] = ['MA-SARUN04-UIND030-MOT:Y-SET']
+indict0['KnobReadback'] = ['MA-SARUN04-UIND030-MOT:Y-SET']
+indict0['KnobTolerance'] = [0.01]
+indict0['KnobWaiting'] = [10]
+indict0['ScanRange'] = [[-0.2, 0.2]]
+indict0['Nstep'] = 4
 
+outdict = pyscan.initializeScan([indict0, indict1])
 
-indict1['PreAction']=[['MA-SARUN04-MQUA020-MOT:X','MA-SARUN04-MQUA020-MOT:X',1,0.1,10]]
-#indict1['PreActionWaiting']=0
-#indict1['PreActionOrder']=0
+print(outdict['ErrorMessage'])
+print(indict1['KnobExpanded'])
 
-indict1['PostAction']=[['MA-SARUN04-MQUA020-MOT:X','MA-SARUN04-MQUA020-MOT:X',0,0,10]]
+outdict = pyscan.startScan()
 
-indict0={}
-indict0['Knob']=['MA-SARUN04-UIND030-MOT:Y-SET']
-indict0['KnobReadback']=['MA-SARUN04-UIND030-MOT:Y-SET']
-indict0['KnobTolerance']=[0.01]
-indict0['KnobWaiting']=[10]
-indict0['ScanRange']=[[-0.2,0.2]]
-indict0['Nstep']=4
+for o in outdict['Observable']:
+    print(o)
 
-outdict=pyscan.initializeScan([indict0,indict1])
+# print pyscan.outdict['KnobReadback']
+outdict = pyscan.initializeScan([indict0, indict1])
+outdict = pyscan.startScan()
 
-print outdict['ErrorMessage']
-print indict1['KnobExpanded']
-
-outdict=pyscan.startScan()
-
-
-for o in  outdict['Observable']:
-    print o
-
-
-#print pyscan.outdict['KnobReadback']
-outdict=pyscan.initializeScan([indict0,indict1])
-outdict=pyscan.startScan()
-
-
-#for o in  outdict['Observable']:
+# for o in  outdict['Observable']:
 #    print o
 
-
-
-print outdict['Validation']
+print(outdict['Validation'])

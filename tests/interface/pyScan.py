30d487c40ed17abe55e3431dc663813dab125a75
Andrej Babic <andrej.babic@psi.ch>
2017-04-17 19:19:36 +0200

    Test the knob readbacks

--- tests/interface/pyScan.py
+++ tests/interface/pyScan.py
@@ -9,8 +9,8 @@ class PyScan(unittest.TestCase):
     def test_initializeScan(self):
         indict1 = dict()
         indict1['Knob'] = ["1.1", "1.2"]
-        indict1['ScanRange'] = [[-2, 0], [-2, 0]]
-        indict1['Nstep'] = 3
+        indict1['ScanRange'] = [[-3, 0], [-3, 0]]
+        indict1['Nstep'] = 4
         indict1['Observable'] = ["READ1", "READ2"]
         indict1['Waiting'] = 0.1
 
@@ -26,10 +26,28 @@ class PyScan(unittest.TestCase):
         pyscan.initializeScan([indict1, indict2], testDal)
         result = pyscan.startScan()
 
-        expected_positions = [[-2, -2, 0, 0], [-2, -2, 1, 1], [-2, -2, 2, 2],
+        expected_positions = [[-3, -3, 0, 0], [-3, -3, 1, 1], [-3, -3, 2, 2],
+                              [-2, -2, 0, 0], [-2, -2, 1, 1], [-2, -2, 2, 2],
                               [-1, -1, 0, 0], [-1, -1, 1, 1], [-1, -1, 2, 2],
                               [-0, -0, 0, 0], [-0, -0, 1, 1], [-0, -0, 2, 2]]
 
         # First 4 PVs are readbacks, the first read is NULL.
         sampled_positions = [single_position[:4] for single_position in testDal.positions[1:]]
-        self.assertEqual(sampled_positions, expected_positions)
+        self.assertEqual(sampled_positions, expected_positions,
+                         "The expected positions do not match the one read by the mock dal.")
+
+        knob_readbacks = result["KnobReadback"]
+
+        self.assertEqual(len(knob_readbacks), indict1['Nstep'],
+                         "The number of steps do not match with the first dimension.")
+        self.assertEqual(len(knob_readbacks[0]), indict2["Nstep"],
+                         "The number of steps do not match with the second dimension.")
+
+        knob_readbacks_expanded = []
+        for knobs in knob_readbacks:
+            knob_readbacks_expanded.extend(knobs)
+
+        self.assertEqual(knob_readbacks_expanded, expected_positions,
+                         "The knob readback values do not match the expected one.")
+
+

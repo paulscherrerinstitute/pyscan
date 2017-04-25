f39257b5a686d1a331b087e298e368a6f955b76a
Andrej Babic <andrej.babic@psi.ch>
2017-04-25 14:03:24 +0200

    Expand test server with readback values

--- tests/epics_test_server.py
+++ tests/epics_test_server.py
@@ -7,11 +7,17 @@ from pcaspy import Driver, SimpleServer
 class MotorTestDriver(Driver):
     prefix = 'PYSCAN:TEST:'
     pvdb = {
-        'MOTOR1': {},
-        'MOTOR2': {},
-        'MOTOR3': {},
-        'MOTOR4': {},
-        'MOTOR5': {}
+        'MOTOR1:SET': {},
+        'MOTOR1:GET': {},
+        'MOTOR2:SET': {},
+        'MOTOR2:GET': {},
+        'MOTOR:PRE1:SET': {},
+        'MOTOR:PRE1:GET': {},
+        'MOTOR:POST1': {},
+        'VALID1': {},
+        'VALID2': {},
+        'OBS1': {},
+        'OBS2': {}
     }
 
     ALL_MOTORS = ['PYSCAN:TEST:' + motor_name for motor_name in pvdb.keys()]
@@ -21,26 +27,41 @@ class MotorTestDriver(Driver):
         self.values = {}
 
     def read(self, reason):
-        return self.values.get(reason, 0)
+        return self.values.get(reason, reason)
 
     def write(self, reason, value):
-        thread = Thread(target=self.move_slowly, args=(reason, value))
-        thread.daemon = True
-        thread.start()
+        # Only motors should move slowly.
+        print(reason)
+        if reason.startswith("MOTOR"):
+            thread = Thread(target=self.move_slowly, args=(reason, value))
+            thread.daemon = True
+            thread.start()
+        else:
+            self.values[reason] = value
 
     def move_slowly(self, attribute, value, steps=10, latency=0.2):
         current_value = self.values.get(attribute, 0)
         step_size = (value - current_value) / steps
 
+        readback = None
+        if attribute.endswith("SET"):
+            readback = attribute.replace("SET", "GET")
+
         for _ in range(steps):
             current_value += step_size
             self.values[attribute] = current_value
+            if readback is not None:
+                self.values[readback] = current_value
+            print(self.values)
             sleep(latency)
 
 
-def run(driver_class):
+def run(driver_class, prefix=None, pvdb=None):
+    prefix = driver_class.prefix if prefix is None else prefix
+    pvdb = driver_class.pvdb if pvdb is None else pvdb
+
     server = SimpleServer()
-    server.createPV(driver_class.prefix, driver_class.pvdb)
+    server.createPV(prefix, pvdb)
     driver = driver_class()
 
     # process CA transactions

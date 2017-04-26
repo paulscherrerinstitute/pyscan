e0bc152fbdf9ce20351634f5476f1caf3889cfe7
Andrej Babic <andrej.babic@psi.ch>
2017-04-26 16:50:42 +0200

    Add pause and abort capabilities.

--- pyscan/scan.py
+++ pyscan/scan.py
@@ -5,6 +5,10 @@ class Scanner(object):
     """
     Perform discrete and continues scans.
     """
+
+    # Interval to sleep while in pause.
+    pause_sleep_interval = 1
+
     def __init__(self, positioner, writer, data_processor, reader, before_executer=None, after_executer=None,
                  initialization_executer=None, finalization_executer=None):
         """
@@ -26,6 +30,7 @@ class Scanner(object):
         self.finalization_executer = finalization_executer
 
         self._user_abort_scan_flag = False
+        self._user_pause_scan_flag = False
 
     def abort_scan(self):
         """
@@ -33,19 +38,41 @@ class Scanner(object):
         """
         self._user_abort_scan_flag = True
 
+    def pause_scan(self):
+        """
+        Pause the scan after the next measurement.
+        """
+        self._user_pause_scan_flag = True
+
+    def resume_scan(self):
+        """
+        Resume the scan.
+        """
+        self._user_pause_scan_flag = False
+
     def setup_monitors(self):
         pass
 
     def _verify_scan_status(self):
+        """
+        Check if the conditions to pause or abort the scan are met.
+        :raise Exception in case the conditions are met.
+        """
+        # Check if the abort flag is set.
         if self._user_abort_scan_flag:
             raise Exception("User aborted scan.")
+
+        # If the scan is in pause, wait until it is resumed or the user aborts the scan.
+        while self._user_pause_scan_flag:
+            if self._user_abort_scan_flag:
+                raise Exception("User aborted scan in pause.")
+            sleep(self.pause_sleep_interval)
 
     def discrete_scan(self, settling_time=0):
         """
         Perform a discrete scan - set a position, read, continue. Return value at the end.
         :param settling_time: Interval between the writing of the position and the reading of data. Default = 0.
         """
-
         try:
             # Set up the experiment.
             if self.initialization_executer:
@@ -64,12 +91,12 @@ class Scanner(object):
                 position_data = self.reader.read()
                 self.data_processor.process(position_data)
 
-                # Verify is the scan should continue.
-                self._verify_scan_status()
-
                 # Post reading callbacks.
                 if self.after_executer:
                     self.after_executer(self)
+
+                # Verify is the scan should continue.
+                self._verify_scan_status()
         finally:
             # Clean up after yourself.
             if self.finalization_executer:

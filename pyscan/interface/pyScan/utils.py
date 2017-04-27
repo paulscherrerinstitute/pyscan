945d117ae17194a2b99517e0fa687d8063d83a09
Andrej Babic <andrej.babic@psi.ch>
2017-04-27 18:55:35 +0200

    Bug fixes in data processor
    PyScan data processing is complicated - it still does not
    work 100% like it should.

--- pyscan/interface/pyScan/utils.py
+++ pyscan/interface/pyScan/utils.py
@@ -1,4 +1,5 @@
-from time import sleep
+
+from pyscan.utils import flatten_list
 
 
 class AbortMonitor(object):
@@ -15,58 +16,40 @@ class AbortMonitor(object):
 
 
 class PyScanDataProcessor(object):
-    def __init__(self, output, n_readbacks, n_validations, n_observable, waiting):
+    def __init__(self, output, n_readbacks, n_validations, n_observables, n_measurements):
         self.n_readbacks = n_readbacks
         self.n_validations = n_validations
-        self.n_observable = n_observable
+        self.n_observables = n_observables
+        self.n_measurements = n_measurements
         self.output = output
-        self.waiting = waiting
-        self.KnobReadback_output_position = self.output_position_generator(self.output["KnobReadback"])
-        self.Validation_output_position = self.output_position_generator(self.output["Validation"])
-        self.Observable_output_position = self.output_position_generator(self.output["Observable"])
-
-    @staticmethod
-    def output_position_generator(list_to_iterate):
-        """
-        Since we are pre-allocating the output, we need a "simple" way to know which element is the next to 
-        write to.
-        :param list_to_iterate: Pre allocated list to iterate over.
-        :return: Generator to iterate over the list.
-        """
-        def flatten(list_to_flatten):
-            # If we reached a list of 0 length, this is our next position to write.
-            if len(list_to_flatten) == 0:
-                yield list_to_flatten
-            # Otherwise we have to go deeper.
-            else:
-                for inner_list in list_to_flatten:
-                    yield from flatten(inner_list)
-
-        return flatten(list_to_iterate)
+        self.KnobReadback_output_position = flatten_list(self.output["KnobReadback"])
+        self.Validation_output_position = flatten_list(self.output["Validation"])
+        self.Observable_output_position = flatten_list(self.output["Observable"])
 
     def process(self, data):
-        if self.n_readbacks == 1:
-            readback_result = data[0]
+        # Just we can always iterate over it.
+        if self.n_measurements == 1:
+            data = [data]
+
+        # Cells for each measurement are already prepared.
+        readback_result = [measurement[0:self.n_readbacks]
+                           for measurement in data]
+        validation_result = [measurement[self.n_readbacks:self.n_readbacks + self.n_validations]
+                             for measurement in data]
+
+        interval_start = self.n_readbacks + self.n_validations
+        interval_end = self.n_readbacks + self.n_validations + self.n_observables
+        observable_result = [measurement[interval_start:interval_end]
+                             for measurement in data]
+
+        if self.n_measurements == 1:
+            next(self.KnobReadback_output_position).extend(readback_result[0])
+            next(self.Validation_output_position).extend(validation_result[0])
+            next(self.Observable_output_position).extend(observable_result[0])
         else:
-            readback_result = data[0:self.n_readbacks]
-
-        if self.n_validations == 1:
-            validation_result = data[self.n_readbacks]
-        else:
-            validation_result = data[self.n_readbacks:self.n_readbacks + self.n_validations]
-
-        if self.n_observable:
-            observable_result = data[-1]
-        else:
-            interval_start = self.n_readbacks + self.n_validations
-            interval_end = self.n_readbacks + self.n_validations + self.n_observables
-            observable_result = data[interval_start:interval_end]
-
-        next(self.KnobReadback_output_position).extend(readback_result)
-        next(self.Validation_output_position).extend(validation_result)
-        next(self.Observable_output_position).extend(observable_result)
-
-        sleep(self.waiting)
+            next(self.KnobReadback_output_position).extend(readback_result)
+            next(self.Validation_output_position).extend(validation_result)
+            next(self.Observable_output_position).extend(observable_result)
 
     def get_data(self):
         return self.output

f60d306430b75b8412da70bf89ae07d7269921b5
Andrej Babic <andrej.babic@psi.ch>
2017-05-02 15:22:14 +0200

    Add integration tests PyScanTool

--- tests/integration.py
+++ tests/integration.py
@@ -1,14 +1,16 @@
+import threading
 import unittest
+from time import sleep
 
 import numpy as np
 
 from tests.interface.old_scan import Scan as CurrentScan
 from tests.utils import TestPyScanDal as CurrentMockDal
 
-
 # Comment this 2 lines to test with the old dal.
 from pyscan.interface.pyScan import Scan as CurrentScan, np
 from tests.mock_epics_dal import MockPyEpicsDal as CurrentMockDal
+
 
 class IntegrationTests(unittest.TestCase):
     def test_EmitMeasTool(self):
@@ -44,7 +46,7 @@ class IntegrationTests(unittest.TestCase):
         indict1['NumberOfMeasurements'] = int(Images)
 
         # Monitor setup
-        # only doing the measurement if laser is producing beam, for the moment we do that the frequency is close to 10 Hz
+        # only doing the measurement if laser is producing beam, we do that the frequency is close to 10 Hz
         indict1['Monitor'] = ['PYSCAN:TEST:MONITOR1']
         indict1['MonitorValue'] = [10]
         indict1['MonitorTolerance'] = [0.1]
@@ -57,7 +59,7 @@ class IntegrationTests(unittest.TestCase):
         # removing the screen after doing the measurement, to be added possible cycling
         indict1['PostAction'] = [["PYSCAN:TEST:PRE1:SET", "PYSCAN:TEST:PRE1:GET", 0, 0, 10], 'Restore']
 
-        outdict = pyscan.initializeScan(indict1, dal=test_dal)
+        pyscan.initializeScan(indict1, dal=test_dal)
         outdict = pyscan.startScan()
 
         if int(Images) == 1:
@@ -106,3 +108,82 @@ class IntegrationTests(unittest.TestCase):
         self.assertTrue(all(abs(i1 - i2) < 0.0001 for i1, i2 in zip(jity, [0, 0, 0, 0])), "Unexpected result.")
         self.assertTrue(all(abs(i1 - i2) < 0.0001 for i1, i2 in zip(rel_jitx, [0, 0, 0, 0])), "Unexpected result.")
         self.assertTrue(all(abs(i1 - i2) < 0.0001 for i1, i2 in zip(rel_jity, [0, 0, 0, 0])), "Unexpected result.")
+
+    def test_PyScanTool(self):
+
+        knob = "PYSCAN:TEST:MOTOR1:SET"
+        instrument = "PYSCAN:TEST:OBS1"
+        numberOfReps = 3
+        scanValues = [0, 1, 2, 3]
+
+        indict = {}
+        indict['Knob'] = knob
+        indict['KnobWaiting'] = 0.1  # later set by expert panel...
+        indict['KnobWaitingExtra'] = 0.2  # later set by expert panel...
+        indict['Waiting'] = 0.5  # later set by expert panel...
+
+        indict['Observable'] = instrument
+        indict['ScanValues'] = scanValues
+        indict['NumberOfMeasurements'] = numberOfReps
+
+        # restore initial set after measurement:
+        indict['PostAction'] = 'Restore'
+
+        test_dal = CurrentMockDal(pv_fixed_values={"PYSCAN:TEST:OBS1": [0.9, 1.0, 1.1]})
+        pyscan = CurrentScan()
+
+        # Check if the progress bar works.
+        def monitor_scan():
+            # make sure the initialization is done:
+            while pyscan.ProgDisp.Progress:
+                sleep(1)
+
+            current_value = 0
+            while current_value < 100:
+                last_value = pyscan.ProgDisp.Progress
+                if last_value > current_value:
+                    progress_values.append(pyscan.ProgDisp.Progress)
+                    current_value = last_value
+            else:
+                nonlocal progress_completed
+                progress_completed = True
+        progress_values = []
+        progress_completed = False
+        threading.Thread(target=monitor_scan).start()
+
+        pyscan.initializeScan(indict, dal=test_dal)
+        outdict = pyscan.startScan()
+
+        # Wait for the progress thread to terminate.
+        sleep(0.2)
+
+        self.assertTrue(progress_completed, "Progress bar did not complete.")
+        self.assertListEqual(progress_values, [25, 50, 75, 100], "The completed percentage is wrong.")
+
+        scanResultKnob = outdict['KnobReadback']
+        scanResultInst = outdict['Observable']
+
+        # remove empty fields:
+        scanResultKnob_clean = [x for x in scanResultKnob if x]
+        scanResultInst_clean = [y for y in scanResultInst if y]
+
+        self.assertListEqual(scanResultKnob, scanResultKnob_clean, "Scan knob lists are not identical.")
+        self.assertListEqual(scanResultInst, scanResultInst_clean, "Instrument lists are not identical.")
+
+        # mean and standard deviation via numpy arrays
+        scanResultKnobMean = [np.array(x).mean() for x in scanResultKnob]
+        scanResultInstMean = [np.array(y).mean() for y in scanResultInst]
+        scanResultInstStd = [np.array(y).std() for y in scanResultInst]
+
+        scanResultArray = np.array(scanResultInst)
+        scanResultMean = [x.mean() for x in scanResultArray]
+        scanResultStd = [x.std() for x in scanResultArray]
+
+        self.assertListEqual(scanResultKnobMean, [0, 1, 2, 3], "The scan knobs result is not the same.")
+        self.assertListEqual(scanResultInstMean, [1, 1, 1, 1], "The instrument mean values do not match.")
+        self.assertListEqual(scanResultMean, [1, 1, 1, 1], "The instrument mean values do not match.")
+        self.assertListEqual(scanResultInstStd, scanResultStd, "Standard deviation results are not the same.")
+
+        if numberOfReps == 3:
+            self.assertTrue(all(abs(i1 - i2) < 0.0001 for i1, i2 in zip(scanResultInstStd, [0.081649] * 4)),
+                            "Unexpected result for standard deviation.")

4765ebe8948d28495c30862649015ff8c1fc4221
Andrej Babic <andrej.babic@psi.ch>
2017-05-05 12:38:12 +0200

    Externalize dal layer
    In order to allow easy mocking of DAL, the classes to use
    are now module level variables.

--- pyscan/scan.py
+++ pyscan/scan.py
@@ -3,6 +3,10 @@ from pyscan.scanner import Scanner
 from pyscan.scan_parameters import EPICS_PV, EPICS_MONITOR, BS_PROPERTY, BS_MONITOR
 from pyscan.utils import convert_to_list, SimpleDataProcessor, SimpleExecutor, compare_channel_value
 
+EPICS_WRITER = epics_dal.WriteGroupInterface
+EPICS_READER = epics_dal.ReadGroupInterface
+BS_READER = bsread_dal.ReadGroupInterface
+
 
 def scan(positioner, writables, readables, monitors=None, initializations=None, finalization=None, settings=None):
     # Allow a list or a single value to be passed.
@@ -77,24 +81,24 @@ def _initialize_epics_dal(writables, readables, monitors, settings):
     epics_monitors_pv_names = [x.pv_name for x in filter(lambda x: isinstance(x, EPICS_MONITOR), monitors)]
 
     # Instantiate the PVs to move the motors.
-    epics_writer = epics_dal.WriteGroupInterface(pv_names=[pv.pv_name for pv in writables],
-                                                 readback_pv_names=[pv.readback_pv_name for pv in writables],
-                                                 tolerances=[pv.tolerance for pv in writables],
-                                                 timeout=settings.write_timeout)
+    epics_writer = EPICS_WRITER(pv_names=[pv.pv_name for pv in writables],
+                                readback_pv_names=[pv.readback_pv_name for pv in writables],
+                                tolerances=[pv.tolerance for pv in writables],
+                                timeout=settings.write_timeout)
 
     # Reading epics PV values.
     epics_pv_reader = None
     if epics_readables_pv_names:
-        epics_pv_reader = epics_dal.ReadGroupInterface(pv_names=epics_readables_pv_names,
-                                                       n_measurements=settings.n_measurements,
-                                                       waiting=settings.measurement_interval)
+        epics_pv_reader = EPICS_READER(pv_names=epics_readables_pv_names,
+                                       n_measurements=settings.n_measurements,
+                                       waiting=settings.measurement_interval)
 
     # Reading epics monitor values.
     epics_monitor_reader = None
     if epics_monitors_pv_names:
-        epics_monitor_reader = epics_dal.ReadGroupInterface(pv_names=epics_monitors_pv_names,
-                                                            n_measurements=1,
-                                                            waiting=0)
+        epics_monitor_reader = EPICS_READER(pv_names=epics_monitors_pv_names,
+                                            n_measurements=1,
+                                            waiting=0)
 
     return epics_writer, epics_pv_reader, epics_monitor_reader
 
@@ -105,7 +109,6 @@ def _initialize_bs_dal(readables, monitors):
 
     bs_reader = None
     if bs_readables or bs_monitors:
-        bs_reader = bsread_dal.ReadGroupInterface(properties=bs_readables,
-                                                  monitor_properties=bs_monitors)
+        bs_reader = BS_READER(properties=bs_readables, monitor_properties=bs_monitors)
 
     return bs_reader

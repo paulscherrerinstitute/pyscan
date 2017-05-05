a02d819f12bf3ef09ca0275a034cb97f4f0214be
Andrej Babic <andrej.babic@psi.ch>
2017-05-05 16:29:15 +0200

    Fix scanner test

--- tests/scanner.py
+++ tests/scanner.py
@@ -15,12 +15,12 @@ class ScannerTests(unittest.TestCase):
         positioner = VectorPositioner(test_positions)
 
         target_positions = []
-        writer = TestWriter(target_positions)
+        writer = TestWriter(target_positions).write
 
         data_processor = SimpleDataProcessor()
 
         read_buffer = [0, 11, 22, 33, 44, 55]
-        reader = TestReader(read_buffer)
+        reader = TestReader(read_buffer).read
 
         scanner = Scanner(positioner, writer, data_processor, reader)
         scanner.discrete_scan()
@@ -28,12 +28,8 @@ class ScannerTests(unittest.TestCase):
         self.assertEqual(target_positions, test_positions,
                          "The output positions are not equal to the input positions.")
 
-        self.assertEqual(read_buffer, [data for position, data in data_processor.get_data()],
+        self.assertEqual(read_buffer, data_processor.get_data(),
                          "The provided and received data is not the same.")
 
-        self.assertEqual(test_positions, [position for position, data in data_processor.get_data()],
+        self.assertEqual(test_positions, data_processor.positions,
                          "The provided and sampled positions are not the same.")
-
-    def test_ScannerCallbacks(self):
-        # TODO: Implement tests callbacks.
-        pass

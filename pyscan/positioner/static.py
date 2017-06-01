b068c730202ad530f88a86f0dc951afe55e13d99
Andrej Babic <andrej.babic@psi.ch>
2017-06-01 14:08:42 +0200

    Fix typo

--- pyscan/positioner/static.py
+++ pyscan/positioner/static.py
@@ -2,7 +2,7 @@
 class StaticPositioner(object):
     def __init__(self, n_images):
         """
-        Acquire N consequtive images in a static position.
+        Acquire N consecutive images in a static position.
         :param n_images: Number of images to acquire.
         """
         self.n_images = n_images

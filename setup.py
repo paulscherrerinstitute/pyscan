db0f25212ce87a4a1f7109d856e3b14c0d82fe81
Andrej Babic <andrej.babic@psi.ch>
2017-06-02 13:42:17 +0200

    Increase library version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyscan',
-    version="2.1.7",
+    version="2.1.8",
     description="PyScan is a python class that performs a scan for single or multiple given knobs.",
     author='Paul Scherrer Institute',
     requires=["numpy", 'pcaspy'],

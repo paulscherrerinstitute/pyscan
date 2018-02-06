9a303346108ac4d2804bee65f4d377332fea7eb2
Andrej Babic <andrej.babic@psi.ch>
2018-02-06 12:47:44 +0100

    Fix pshell integration tests

--- tests/helpers/mock_pshell_function.py
+++ tests/helpers/mock_pshell_function.py
@@ -7,12 +7,13 @@ class MockPShellFunction(PShellFunction):
 
     def _execute_scan(self, execution_parameters):
         # Example data from the test scan.
-        return '[0.3627235580357685,"2018/02/05/20180205_173200_DataLink.h5 | scan 1/Sensor"]'
+        return '[2,"2018/02/06/20180206_122248_DataLink.h5 | scan 1/Sensor"]'
 
     def _load_scan_data(self, data_path):
         # Example data from the test scan.
-        return b'\x00\x00\x00\x89{"htype":"bsr_m-1.1","hash":"f7977dd9531f6d52f42371342ea656a8",' \
-               b'"global_timestamp":{"sec":1860483,"ns":552702938},"dh_compression":"none"}\x00\x00\x00>' \
-               b'{"htype":"bsr_d-1.1","channels":[{"name":"data","shape":[2]}]}\x00\x00\x00\x10\xe9D\x85\x1e8' \
-               b'\xc7\xea\xbf\xc3\xa8\xa0\x0f0\xfc\xe7\xbf\x00\x00\x00\x10\x96\x87xZ\x00\x00\x00\x00\xc0V\xfe\x03\
-               x00\x00\x00\x00'
+        return b'\x00\x00\x00\x87{"htype":"bsr_m-1.1","hash":"85877517a3f81ebcf617b657b87a5a5",' \
+               b'"global_timestamp":{"sec":1929316,"ns":80742626},"dh_compression":"none"}\x00\x00\x00>' \
+               b'{"htype":"bsr_d-1.1","channels":[{"name":"data","shape":[6]}]}\x00\x00\x000\x00\x00\x00\x00\x00' \
+               b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00' \
+               b'\x08@\x00\x00\x00\x00\x00\x00\x10@\x00\x00\x00\x00\x00\x00\x14@\x00\x00\x00\x10v\x94yZ\x00\x00' \
+               b'\x00\x00\xc0\xfav#\x00\x00\x00\x00'
--- tests/test_pshell_integration.py
+++ tests/test_pshell_integration.py
@@ -9,10 +9,12 @@ class PshellIntegration(unittest.TestCase):
 
     def test_pshell_function(self):
 
+        n_steps = 5
+
         pshell_test = MockPShellFunction(script_name="test/DataLink.py",
                                          parameters={"scan_start": 0,
                                                      "scan_stop": 10,
-                                                     "scan_steps": 10},
+                                                     "scan_steps": n_steps},
                                          scan_in_background=False)
 
         result = pshell_test.read()
@@ -21,14 +23,19 @@ class PshellIntegration(unittest.TestCase):
 
         self.assertTrue(isinstance(result, dict))
 
-        self.assertEqual(1, len(result))
+        self.assertEqual(n_steps + 1, len(result["data"]))
+
+        for index in range(n_steps + 1):
+            result["data"][index] == float(index)
 
     def test_pshell_function_in_scan(self):
+
+        n_steps = 5
 
         pshell_test = MockPShellFunction(script_name="test/DataLink.py",
                                          parameters={"scan_start": 0,
                                                      "scan_stop": 10,
-                                                     "scan_steps": 10},
+                                                     "scan_steps": n_steps},
                                          scan_in_background=False)
 
         n_positions = 5
@@ -40,3 +47,8 @@ class PshellIntegration(unittest.TestCase):
                       readables=readables)
 
         self.assertEqual(n_positions, len(result))
+
+        for position_index in range(n_positions):
+            for value_index in range(n_steps + 1):
+                result[position_index][0]["data"][value_index] == float(value_index)
+

b04fd9ad5461ac0229c0fbc8725195c3201ceecd
Andrej Babic <andrej.babic@psi.ch>
2018-11-21 16:19:35 +0100

    New library version
    - Add support for operation on EPICS conditions.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyscan',
-    version="2.7.1",
+    version="2.8.0",
     description="PyScan is a python class that performs a scan for single or multiple given knobs.",
     author='Paul Scherrer Institute',
     requires=["numpy", 'pcaspy', 'requests'],

08fe86dfa05945c310d1899d11f9d1ab592ae1f5
Andrej Babic <andrej.babic@psi.ch>
2018-11-21 11:24:53 +0100

    New library version
    - Fix travis build.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyscan',
-    version="2.7.0",
+    version="2.7.1",
     description="PyScan is a python class that performs a scan for single or multiple given knobs.",
     author='Paul Scherrer Institute',
     requires=["numpy", 'pcaspy', 'requests'],
